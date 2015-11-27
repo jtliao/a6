6486213885f9d8ae9f6f55af94b8ab6648dae7f2
jtliao <jtl93@cornell.edu>
2015-11-27 12:37:36 -0500

    Made some more structural changes to main

--- main.ml
+++ main.ml
@@ -1,7 +1,6 @@
-open Dict
 open IOfile
 
-type constraint =
+type operator =
   |Eq of string * wrapper
   |Lt of string * wrapper
   |Gt of string * wrapper
@@ -9,12 +8,17 @@ type constraint =
   |GtEq of string * wrapper
   |NotEq of string * wrapper
 
+type constraint =
+  |Op of operator
+  |And of operator * constraint
+  |Or of operator * constraint
+
 type command =
-  |Select of string list*string*(constraint list option)
-  |Update of string * string * (constraint list option)
-  |Delete of string * (constraint list option)
-  |Insert of string * (string list) * (wrapper list)
-  |Create of string * (string list)
+  |Select of string list * string * constraint option
+  |Update of string * string list* constraint
+  |Delete of string * constraint
+  |Insert of string * string list * wrapper list
+  |Create of string * string list
   |Drop of string
 
 let fst = function
@@ -28,7 +32,7 @@ let trd = function
 
 (*Parses the string that the user inputs into a type command*)
 let parse_input (s:string) : command =
-  if String.
+  failwith "TODO"
 
 let rec get_col_indices (cols:string list) (d: dict) : int list =
   match cols with
@@ -37,30 +41,34 @@ let rec get_col_indices (cols:string list) (d: dict) : int list =
             |Some x -> x::(get_col_indices t d)
             |None -> get_col_indices t d)
 
-let execute_select (cols : string list) (tab: string)
-(dicts:dict * dict * dict) : unit =
+let execute_select (cols : string list) (tab: string) (cons: constraint)
+(dicts: ('a*'b) Hashtbl.t * ('c*'d) Hashtbl.t * ('e*'f) Hashtbl.t) : unit =
   let table_dicts = match Dict.lookup (fst dicts) tab with
                     |Some x -> x
                     |None -> failwith "doesnt exist" in
   let index_list = get_col_indices cols (snd dicts) in
 
-let execute_update =
+let execute_update =failwith "TODO"
 
-let execute_delete =
+let execute_delete =failwith "TODO"
 
-let execute_insert =
+let execute_insert =failwith "TODO"
 
-let execute (com:command) (dicts: dict * dict * dict) : dict * dict * dict =
+let execute_create =failwith "TODO"
+
+let execute (com:command) (dicts: ('a*'b) Hashtbl.t * ('c*'d) Hashtbl.t *
+  ('e*'f) Hashtbl.t):('a*'b) Hashtbl.t * ('c*'d) Hashtbl.t * ('e*'f) Hashtbl.t =
   match com with
   |Select (col, tab, None) -> execute_select col tab dicts; dicts
   |Select (col, tab, Some cons) -> execute_select col tab dicts; dicts
   |Update () -> execute_update
   |Delete () -> execute_delete
   |Insert () -> execute_insert
-  |Create ()
+  |Create () -> execute_create
   |Drop t -> (remove t (fst dicts), snd dicts, trd dicts)
 
-let rec run_repl (dicts: dict * dict * dict) : unit =
+let rec run_repl (dicts: ('a*'b) Hashtbl.t * ('c*'d) Hashtbl.t *
+  ('e*'f) Hashtbl.t) : unit =
   let user_command = read_line () in
   run_repl (execute (parse_input user_command) dicts)
 
--- main.mli
+++ main.mli
@@ -1,32 +1,57 @@
-open Dict
 open IOfile
 
 (*Type to represent the different possibilities for constraints in the WHERE
-  statement*)
-type constraint =
-  |Eq of col * wrapper
-  |Lt of col * wrapper
-  |Gt of col * wrapper
+  statement, where string is the field name and wrapper is the type of
+  the element*)
+type operator =
+  |Eq of string * wrapper
+  |Lt of string * wrapper
+  |Gt of string * wrapper
+  |LtEq of string * wrapper
+  |GtEq of string * wrapper
+  |NotEq of string * wrapper
 
-(*Select (col, tab, Some constraint) indicates the command SELECT col FROM tab
-  WHERE constraint. *)
+(*Type to create a constraint using a single operator, an And combining two
+  operators or more, or an Or combining two operators or more*)
+type constraint =
+  |Op of operator
+  |And of operator * constraint
+  |Or of operator * constraint
+
+(*-Select (col, t, [None]) indicates the command SELECT col FROM t
+  -Select (col, t, [Some constraint]) indicates the command SELECT col FROM
+   t WHERE constraint
+  -Update (t, [c1;c2], [v1;v2], cons) indicates the command UPDATE t SET c1=v1,
+   c2=v2 WHERE cons
+  -Delete (t, cons) is the command DELETE FROM t WHERE cons
+  -Insert (t, [c1;c2], [v1;v2]) inserts v1 into c1, v2 into c2 in table t
+  -Create (t, [c1;c2;...] creates table t with columns c1, c2,...)
+  -Drop t drops the table t*)
 type command =
-  |Select of string*string*(constraint option)
-  |Table of
+  |Select of string list * string * constraint option
+  |Update of string * string list* constraint
+  |Delete of string * constraint
+  |Insert of string * string list * wrapper list
+  |Create of string * string list
+  |Drop of string
 
 (*Parses the string that the user inputs into a type command*)
 val parse_input : string -> command
 
-(*Acts on the command that is inputted, taking the pair of current
-  database data structures, with the first dict being the name-index dict and
-  the second being the index-array dict, as parameters and returning the
+(*Acts on the command that is inputted, taking the tuple of current
+  database data structures (with the first hash table being the table name ->
+  table values table, the second being the column name -> index table, and the
+  third being the row index -> array table) as parameters and returning the
   updated pair after the command has been executed*)
-val execute : command -> dict * dict -> dict * dict
+val execute : command -> (('a*'b) Hashtbl.t * ('c*'d) Hashtbl.t *
+  ('e*'f) Hashtbl.t) -> (('a*'b) Hashtbl.t * ('c*'d) Hashtbl.t *
+  ('e*'f) Hashtbl.t)
 
 (*The function that will act as the REPL, repreatedly running and passing
-  in the current data structures as parameters, with the first dict in the pair
-  being the name-index dict and the second being the index-array dict*)
-val run_repl : dict * dict -> unit
+  in the current data structures as parameters, with the first hash table being
+  the table name -> table values table, the second being the column name ->
+  index table, and the third being the row index -> array table*)
+val run_repl : (('a*'b) Hashtbl.t * ('c*'d) Hashtbl.t * ('e*'f) Hashtbl.t)->unit
 
 (*The "main" function that will ask for the database file name and import
   it using iofile.ml, then run the REPL*)
