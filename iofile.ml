b199f9be38bddfa274e11444a7ac5f57e00b7789
Chris Mei <kaijiemei@gmail.com>
2015-11-30 14:57:45 -0500

    extlib files

---
+++ iofile.ml
@@ -0,0 +1,19 @@
+open Extlib
+
+type wrapper =
+  |Int of int
+  |Float of float
+  |String of string
+  |Null
+
+let read_file s =
+  failwith "TODO"
+
+let write_file s1 s2 =
+  failwith "TODO"
+
+let string_to_dict s =
+  failwith "TODO"
+
+let dict_to_string d =
+  failwith "TODO"
