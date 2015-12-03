48e0902931273727f2dce78fb477301ee401c582
Chris Mei <kaijiemei@gmail.com>
2015-12-03 10:56:44 -0500

    added comments to functions not in mli

--- iofile.ml
+++ iofile.ml
@@ -24,6 +24,8 @@ let write_file (changes: string) (filename: string): unit =
   output_string chan changes;
   close_out chan
 
+(*Converts a string to its corresponding wrapper. Int and Float strings must
+  contain no extra characters*)
 let string_to_wrap (str: string): wrapper =
   try
     let i = int_of_string str in
@@ -46,6 +48,7 @@ let rec string_to_array (str: string) (arr: wrapper array) (build: string):
     | '\n' -> arr
     | _ -> string_to_array rest arr (build^(Bytes.make 1 char_at))
 
+(*Converts a string array to a (string,int) Hashtbl*)
 let array_to_hashtbl (arr: string array): (string,int) Hashtbl.t =
   let tbl = Hashtbl.create (Array.length arr) in
   for i = 0 to (Array.length arr - 1) do
@@ -53,6 +56,8 @@ let array_to_hashtbl (arr: string array): (string,int) Hashtbl.t =
   done;
   tbl
 
+(*Recursively updates header and rows until the string is empty, then returns
+  the Hashtbl.t tuple (header,rows).*)
 let rec string_to_dict_help (str: string) (header: (string,int) Hashtbl.t)
   (rows: (int, wrapper array) Hashtbl.t) (rownum: int): (string,int) Hashtbl.t *
   (int, wrapper array) Hashtbl.t =
@@ -74,6 +79,7 @@ let string_to_dict (s: string): (string, int) Hashtbl.t *
   (int, wrapper array) Hashtbl.t =
   string_to_dict_help s (Hashtbl.create 0) (Hashtbl.create 0) 0
 
+(*Convert a wrapper array to a string for printing*)
 let array_to_string (arr: wrapper array): string =
   Array.fold_left (fun acc x -> acc^(wrap_to_string x)^"|") "|" arr
 
